c0688d57ddd046626994e9e9e88e6e2925354434
15M4C4R <mkhazni2004@gmail.com>
2025-04-15 12:55:57 +0200

    conferences.db

--- backend/src/main/java/network/backend/model/jpa/GroupRepository.java
+++ backend/src/main/java/network/backend/model/jpa/GroupRepository.java
@@ -2,8 +2,9 @@ package network.backend.model.jpa;
 
 import network.backend.model.Group;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    
 }
